65244468dac59babb33954d6dbb8e4b5efedfd8e
Ramin SOlhi <raminsoli@gmail.com>
2022-12-10 01:03:42 +0330

    20221210

--- GrpcService1/Program.cs
+++ GrpcService1/Program.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Discount.Grpc
+namespace GrpcService1
 {
     public class Program
     {
--- GrpcService1/Services/GreeterService.cs
+++ GrpcService1/Services/GreeterService.cs
@@ -1,9 +1,11 @@
 using Grpc.Core;
-using Discount.Grpc;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Discount.Grpc.Services
+namespace GrpcService1
 {
     public class GreeterService : Greeter.GreeterBase
     {
@@ -21,5 +23,4 @@ namespace Discount.Grpc.Services
             });
         }
     }
-
 }
